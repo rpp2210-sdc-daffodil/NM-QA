198d97fa320625ecaa7142ad01751f53c1cec134
Nick Monteleone <nickmonteleone25@gmail.com>
2023-05-06 13:02:38 -0700

    Added helper function for get answers query

--- server/app.js
+++ server/app.js
@@ -41,8 +41,18 @@ app.get('/qa/questions/:product_id', (req, res, next) => {
 
 app.get('/qa/questions/:question_id/answers', (req, res, next) => {
   const questionID = req.params.question_id;
-  console.log(questionID);
-  res.sendStatus(200);
+  const count = req.query.count ? req.query.count : 5;
+  const page = req.query.page ? req.query.page : 1;
+
+  queries.getAnswers(db, questionID, count, page, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      // console.log(results);
+      res.json(results.rows);
+    }
+  });
 });
 
 app.post('/qa/questions', (req, res, next) => {
--- server/queries.js
+++ server/queries.js
@@ -13,14 +13,13 @@ module.exports = {
             'question_helpfulness', q.question_helpfulness,
             'reported', q.reported,
             'answers', (
-              SELECT json_object_agg(
+              SELECT COALESCE(json_object_agg(
                 a.answer_id, json_build_object(
                   'id', a.answer_id,
                   'body', a.body,
                   'date', a.date_written,
                   'answerer_name', a.answerer_name,
                   'helpfulness', a.helpful,
-
                   'photos', (
                     SELECT COALESCE(json_agg(
                       json_build_object(
@@ -30,7 +29,7 @@ module.exports = {
                     ), '[]'::json) FROM photos p WHERE p.answer_id = a.answer_id
                   )
                 )
-              )
+              ), '[]'::json)
               FROM answers a
               WHERE a.question_id = q.question_id
             )
@@ -39,7 +38,7 @@ module.exports = {
     FROM (
       SELECT
         *,
-        ROW_NUMBER() OVER (ORDER BY question_date DESC) AS rn
+        ROW_NUMBER() OVER (ORDER BY question_id ASC) AS rn
       FROM questions
       WHERE product_id = ${id}
     ) q
@@ -47,6 +46,46 @@ module.exports = {
       q.rn <= ${count}
     GROUP BY
       q.product_id;`,
+      (err, results) => {
+        if (err) {
+          cb(err);
+        } else {
+          cb(null, results);
+        }
+      },
+    );
+  },
+  getAnswers: (db, id, count, page, cb) => {
+    db.query(
+      `SELECT
+        ${id} as question,
+        ${page} as page,
+        ${count} as count,
+        json_agg(
+          json_build_object(
+            'answer_id', a.answer_id,
+            'body', a.body,
+            'date', a.date_written,
+            'answerer_name', a.answerer_name,
+            'helpfulness', a.helpful,
+            'photos', (
+              SELECT COALESCE(json_agg(
+                json_build_object(
+                  'id', p.id,
+                  'url', p.url
+                )
+              ), '[]'::json) FROM photos p WHERE p.answer_id = a.answer_id
+            )
+          )
+        ) as results
+        FROM (
+          SELECT
+            *,
+            ROW_NUMBER() OVER (ORDER BY answer_id ASC) AS rn
+          FROM answers
+          WHERE question_id = ${id}
+        ) a
+        WHERE rn <= ${count}`,
       (err, results) => {
         if (err) {
           cb(err);
